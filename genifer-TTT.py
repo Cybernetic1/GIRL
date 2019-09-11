f042bce11eac11bc0626990b4a71db547f8885cd
Cybernetic1 <generic.intelligence@gmail.com>
2019-09-11 19:55:42 +0800

    minor

--- genifer-TTT.py
+++ genifer-TTT.py
@@ -12,10 +12,14 @@ def init_network():
 	c1 = Has('male', '$a')
 	c2 = Has('love', '$a', '$b')
 	c3 = Has('female', '$b')
-	# net.add_production(Rule(Ncc(c1, Ncc(c2, c3))))
-	net.add_production(Rule(c1, Ncc(c2, c3)))
+	net.add_production(Rule(Ncc(c1, Ncc(c2, c3))))
+	# net.add_production(Rule(c1, Ncc(c2, c3)))
 	# net.add_production(Rule(c2, c3))
 
+	# c01 = Has('O', '$x', '$x')
+	# c02 = Has('□', '$y', '$z')
+	# c03 = Has('>', '$y', '$z')
+	# net.add_production(Rule(c01, c02, c03))
 	return net
 
 def add_wmes(net):
@@ -29,6 +33,15 @@ def add_wmes(net):
 		#WME('love', 'Pete', 'Ann'),
 		WME('male', 'John'),
 		WME('male', 'Pete'),
+		# WME('X', '0', '2'),
+		# WME('X', '1', '1'),
+		# WME('X', '2', '1'),
+		# WME('O', '0', '0'),
+		# WME('O', '1', '0'),
+		# WME('O', '1', '2'),
+		# WME('O', '2', '2'),
+		# WME('□', '0', '1'),
+		# WME('□', '2', '0'),
 	]
 	for wme in wmes:
 		net.add_wme(wme)
